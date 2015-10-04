31423419ba68e084e070c3bee89a597f98da2fe6
Franco <Franco@192.168.2.17>
2015-10-04 01:09:52 -0600

    Primera Version

--- src/java/org/lasmedilas/siates/jpa/controler/JpaControlador.java
+++ src/java/org/lasmedilas/siates/jpa/controler/JpaControlador.java
@@ -6,6 +6,9 @@
 
 package org.lasmedilas.siates.jpa.controler;
 
+import java.util.List;
+import org.lasmedilas.siates.jpa.entidades.Entidad;
+
 /**
  *
  * @author Franco Eder Chacon Linares 
@@ -13,5 +16,37 @@ package org.lasmedilas.siates.jpa.controler;
  * 
  */
 public class JpaControlador implements Iacciones{
+
+    public void guardar(Entidad entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void crear(Entidad entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void editar(Entidad entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void borrar(Entidad entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<?> getEntidades() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<?> getEntidades(String comodin) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Entidad getEntidad(String llave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Entidad getEntidad(Entidad llave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
